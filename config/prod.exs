bfaf3ce1b24ae7fa4dd4a904f9d941a5789be4a1
Doug DeBold <ddebold56@gmail.com>
2017-05-22 11:16:07 -0500

    track secrets file

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "arcane-woodland-15220.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
